b30f5a74bf58636c0ceab733d7a6d329389f1dd7
SunnyLee02 <snunrich02@gmail.com>
2023-01-30 17:58:18 +0900

    mysql db change

--- myapi/settings.py
+++ myapi/settings.py
@@ -87,9 +87,12 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SunnyLee2002$ourmemory',
+        'USER': 'SunnyLee2002',
+        'PASSWORD': '!20snunrich02!',
+        'HOST': 'SunnyLee2002.mysql.pythonanywhere-services.com',
     }
 }
 
